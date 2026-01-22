08b42268d8f1e301bbc1db0e01c53d01c32ba843
Mihail Obrokov <mihailobrokov@yandex.ru>
2026-01-22 21:14:56 +0300

    Убран неиспользуемый код. Поправлены комментарии в README

--- src/main/java/ru/yandex/practicum/utils/DriverPath.java
+++ src/main/java/ru/yandex/practicum/utils/DriverPath.java
@@ -2,13 +2,8 @@ package ru.yandex.practicum.utils;
 
 public class DriverPath {
 
-//    Для запуска тестов укажите путь до исполнительного файла Яндекс Браузера на своем компьютере в YANDEX_BINARY_PATH.
-//    Если тесты запускаются на Win OS - используйте YANDEX_DRIVER_WIN_PATH.
-//    Если тесты запускаются на MAC/Linux - используйте YANDEX_DRIVER_MAC_LINUX_PATH.
-
-
     static final String YANDEX_BINARY_PATH = "C:/Users/Freud/AppData/Local/Yandex/YandexBrowser/Application/browser.exe";
 
     static  final String YANDEX_DRIVER_WIN_PATH = "src/test/resources/yandexdriver.exe";
-//  static final String YANDEX_DRIVER_MAC_LINUX_PATH = "src/test/resources/yandexdriver";
+
 }
